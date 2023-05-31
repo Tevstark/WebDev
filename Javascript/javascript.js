e03fb34f1c3804660f9a60bcac87fb33c1554fb4
Tevstark <tevkamau@gmail.com>
2023-05-31 20:00:29 +0300

    JS Exercise

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,3 +1,19 @@
-let message = 3;
+let a = 10;
+console.log(9*a);
 
-alert(message);
+let b = 7 * a;
+console.log(b);
+
+console.log(23 + 97);
+
+console.log(6+5+8+9+10+5);
+
+console.log((4+6+9)/77);
+
+const max = 57
+
+let actual = max - 13;
+
+let percentage = actual / max;
+
+let nameFieldChecked = true;
