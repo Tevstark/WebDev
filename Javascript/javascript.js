983067a9b55e83e1531ecd5307d2a468513ff986
Tevstark <tevkamau@gmail.com>
2023-06-01 22:40:34 +0300

    JS string exercises

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,19 +1,3 @@
-let a = 10;
-console.log(9*a);
+let petName = "Bones";
 
-let b = 7 * a;
-console.log(b);
-
-console.log(23 + 97);
-
-console.log(6+5+8+9+10+5);
-
-console.log((4+6+9)/77);
-
-const max = 57
-
-let actual = max - 13;
-
-let percentage = actual / max;
-
-let nameFieldChecked = true;
+console.log(petName.split());
