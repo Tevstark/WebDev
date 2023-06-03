99e5e78963f5bb259cab0878a68bf9e8a3661219
Tevstark <tevkamau@gmail.com>
2023-06-03 23:43:42 +0300

    Mobile First Design

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -9,3 +9,29 @@ if (time < 10) {
     console.log("Good Evening Sir")
 }
 
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case  6:
+        day = "Saturday";
+        break;
+}
+
+console.log(`Today is ${day} `);
