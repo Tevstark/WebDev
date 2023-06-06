88544e0e8b37805b53288c2cdb1fba6c794ddfa5
Tevstark <tevkamau@gmail.com>
2023-06-06 13:05:48 +0300

    Grading Script

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,3 +1,22 @@
-const anyName = "Tevin";
+let testScores = 88
+let grade;
+let result;
 
-console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+if (testScores > 100) {
+    console.log("Points not valid")
+}else if (testScores > 60 && testScores < 101) {
+    console.log(result = "Pass");
+    if (testScores >= 90) {
+        grade = `A of ${testScores} points`;
+    } else if (testScores >= 80) {
+        grade = `B of ${testScores} points`;
+    } else if (testScores >= 70) {
+        grade = `C of ${testScores} points`;
+    } else if (testScores >= 60) {
+        grade = `D of ${testScores} points`; 
+    }
+    console.log(grade);
+} else {
+    console.log(result = "FAIL");
+    console.log(`${testScores} points`)
+}
