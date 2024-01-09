5c65edd8534143b8f9b784de15a5acf0d79bafa0
TevStark <tevkamau@gmail.com>
2024-01-09 23:48:02 +0300

    Playing around with methods

--- Javascript/DOM-manipulation/index.js
+++ Javascript/DOM-manipulation/index.js
@@ -1,1 +1,12 @@
-document.body.style.background = "aqua";
+// alert(location.href)
+// if (confirm("Go to Wikipedia?")){
+//     function openNewTab(url){
+//         window.open(url, "_blank");
+//     }
+
+//     openNewTab("http://en.wikipedia.org")
+// }
+
+let changeCardColor = document.getElementsByClassName("card")
+changeCardColor.style.color= "red";
+
