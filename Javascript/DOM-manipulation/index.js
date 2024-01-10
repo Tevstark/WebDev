bafa10b77f4d0893df00163f435487aa75e86554
TevStark <tevkamau@gmail.com>
2024-01-10 23:49:47 +0300

    DOM methods

--- Javascript/DOM-manipulation/index.js
+++ Javascript/DOM-manipulation/index.js
@@ -7,6 +7,24 @@
 //     openNewTab("http://en.wikipedia.org")
 // }
 
-let changeCardColor = document.getElementsByClassName("card")
-changeCardColor.style.color= "red";
+let changeCardColor = document.getElementById("card-prototype")
+changeCardColor.style.color= "green";
 
+let changeTheme = document.getElementsByTagName("button")
+
+changeTheme.style.background= "black";
+
+const parent = document.getElementById('parent').lastElementChild
+console.log(parent) //<p>i am the last child</p>
+
+const parent2 = document.getElementById('parent').children[3]
+console.log(parent2) //<h1>hello world</h1>
+
+const secondchild = document.getElementById('secondchild')
+console.log(secondchild) //<p id="secondchild">i am the second child</p>
+
+console.log(secondchild.parentNode) //<div id="parent">...</div>
+
+console.log(secondchild.nextElementSibling) //<h4>i am alive</h4>
+
+console.log(secondchild.previousElementSibling) //<div id="firstchild">i am a first child</div>
