86bec301d2f1b26f9b4129893de0162dc1d14bff
TevStark <tevkamau@gmail.com>
2024-01-12 23:47:14 +0300

    Fiddling around

--- Javascript/DOM-manipulation/index.js
+++ Javascript/DOM-manipulation/index.js
@@ -7,32 +7,45 @@
 //     openNewTab("http://en.wikipedia.org")
 // }
 
-let changeCardColor = document.getElementById("card-prototype")
-changeCardColor.style.color= "green";
 
-let changeTheme = document.getElementsByTagName("button")
+// // Accessing a button element
+// const myButton = document.getElementById('my-Button');
 
-changeTheme.style.background= "black";
+// // Adding a click event listener
+// myButton.addEventListener('click', function() {
+//     alert('Button Clicked!');
+// });
 
-const parent = document.getElementById('parent').lastElementChild
-console.log(parent) //<p>i am the last child</p>
 
-const parent2 = document.getElementById('parent').children[3]
-console.log(parent2) //<h1>hello world</h1>
+let headingOne = document.createElement("h1");
+headingOne.textContent = "This is my first text.";
+document.body.appendChild(headingOne);
 
-const secondchild = document.getElementById('secondchild')
-console.log(secondchild) //<p id="secondchild">i am the second child</p>
-
-console.log(secondchild.parentNode) //<div id="parent">...</div>
-
-console.log(secondchild.nextElementSibling) //<h4>i am alive</h4>
-
-console.log(secondchild.previousElementSibling) //<div id="firstchild">i am a first child</div>
 
 // Accessing a button element
-const myButton = document.getElementById('myButton');
+const myButton = document.getElementById('my-Button');
 
 // Adding a click event listener
 myButton.addEventListener('click', function() {
-    alert('Button Clicked!');
+    document.body.style.background = 'black'
+    document.body.style.color = 'white';
+    console.log('Button Clicked!')
+
+});
+
+let list = [
+    'Ubuntu',
+    'Kali',
+    'Arch',
+    'Mint'
+];
+
+let olElement = document.createElement('ol');
+
+list.forEach(function(text){
+
+    let li = document.createElement('li');
+    li.innerHTML = `${text}`;
+
+    olElement.appendChild(li);
 });
