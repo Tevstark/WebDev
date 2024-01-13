2e7fe9fa321a679f42ee66b22444a97be1381b2b
TevStark <tevkamau@gmail.com>
2024-01-13 21:55:26 +0300

    Update HTML and CSS for Counter app.

--- Mini-Projects/Counter-app/index.js
+++ Mini-Projects/Counter-app/index.js
@@ -1,4 +1,4 @@
-document.body.style.cssText = 'background:grey; display: flex; flex direction: column; align-items: center; justify-content: center; width: 100vw; height: 100vh;';
+// document.body.style.cssText = 'background:grey; display: flex; flex direction: column; align-items: center; justify-content: center; width: 100vw; height: 100vh;';
 
 
 
@@ -6,9 +6,9 @@ let add = document.querySelector('#add');
 let counterEl = document.querySelector('#counter');
 let subtract = document.querySelector('#subtract');
 
-add.style.cssText = "color: white; background: black; width: 30px; height: 30px; border: 1px solid black; display: flex; justify-content: center; align-items: center; margin: 20px;"
+// add.style.cssText = "color: white; background: black; width: 30px; height: 30px; border: 1px solid black; display: flex; justify-content: center; align-items: center; margin: 20px;"
 
-subtract.style.cssText = "color: white; background: black; width: 30px; height: 30px; border: 1px solid black; display: flex; justify-content: center; align-items: center; margin: 20px;"
+// subtract.style.cssText = "color: white; background: black; width: 30px; height: 30px; border: 1px solid black; display: flex; justify-content: center; align-items: center; margin: 20px;"
 
 let counter = 0;
 add.addEventListener('click', () => {
